d54a1f51864b9b3ed75944b57ba4b024c7235897
gosxiesperanto <g.toyota@gmail.com>
2026-01-28 22:02:08 -0500

    added grocery tracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,16 @@ let area = length * width;
 
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+let grocery1_amount, grocery2_amount, grocery3_amount;
+
+function grocery_tracker() {
+    grocery1_amount = parseFloat(document.getElementById('grocery1').value);
+    grocery2_amount = parseFloat(document.getElementById('grocery2').value);
+    grocery3_amount = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1_amount + grocery2_amount + grocery3_amount;
+
+    document.getElementById('result2').innerText = `The total amount of the grocery items is: ${total}`;    
+}
